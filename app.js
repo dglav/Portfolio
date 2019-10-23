6b54befcd936df60590772f47009bb439863bec5
dglav <glavich_drew@yahoo.com>
2019-10-23 23:03:26 +0900

    Updated to new projects including React ones and refactored menu. Also added more detailed descriptions to CRWN Clothing and Yelpcamp pages.

--- app.js
+++ app.js
@@ -1,60 +1,64 @@
-let express = require('express'),
-    ejs     = require('ejs'),
-    bodyParser = require('body-parser');
-    
-let app = express();
-let language = 'english';
+let express = require("express"),
+  ejs = require("ejs"),
+  bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
+let app = express();
+let language = "english";
+
+app.set("view engine", "ejs");
 
 express.static(__dirname);
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next){
-    res.locals.currentRoute = req.path;
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentRoute = req.path;
+  next();
 });
 
-app.get("/", function(req, res){
-    res.render(`${language}/index`);
+app.get("/", function(req, res) {
+  res.render(`${language}/index`);
 });
 
-app.post("/", function(req, res){
-    language = req.body.language;
-    res.redirect("/");
+app.post("/", function(req, res) {
+  language = req.body.language;
+  res.redirect("/");
 });
 
-app.get("/yelpcamp", function(req, res){
-    res.render(`${language}/projects/yelpcamp`);
+app.get("/crwn-clothing", function(req, res) {
+  res.render(`${language}/projects/crwn-clothing`);
 });
 
-app.get("/omnifood", function(req, res){
-    res.render(`${language}/projects/omnifood`);
+app.get("/yelpcamp", function(req, res) {
+  res.render(`${language}/projects/yelpcamp`);
 });
 
-app.get("/weatherapp", function(req, res){
-    res.render(`${language}/projects/weatherapp`);
+app.get("/omnifood", function(req, res) {
+  res.render(`${language}/projects/omnifood`);
 });
 
-app.get("/colorgame", function(req, res){
-    res.render(`${language}/projects/colorgame`);
+app.get("/weatherapp", function(req, res) {
+  res.render(`${language}/projects/weatherapp`);
 });
 
-app.get("/todolist", function(req, res){
-    res.render(`${language}/projects/todolist`);
+app.get("/colorgame", function(req, res) {
+  res.render(`${language}/projects/colorgame`);
 });
 
-app.get("/fitnessapp", function(req, res){
-    res.render(`${language}/projects/fitnessapp`);
+app.get("/todolist", function(req, res) {
+  res.render(`${language}/projects/todolist`);
 });
 
-app.post("/:project", function(req, res){
-    language = req.body.language;
-    res.redirect(`${req.params.project}`);
+app.get("/fitnessapp", function(req, res) {
+  res.render(`${language}/projects/fitnessapp`);
+});
+
+app.post("/:project", function(req, res) {
+  language = req.body.language;
+  res.redirect(`${req.params.project}`);
 });
 
 let port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log('server started!');
+app.listen(port, function() {
+  console.log("server started!");
 });
